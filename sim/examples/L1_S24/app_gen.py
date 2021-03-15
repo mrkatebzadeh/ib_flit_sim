91cc139428625705f7962afffeb899b29b707c0e
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2021-03-15 11:58:26 +0000

    Added L1_S24

---
+++ sim/examples/L1_S24/app_gen.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+import sys
+
+active_app = sys.argv[1]
+
+template = """
+### App: {0}
+**H_{0}_U1.app.disable  = {1}
+**H_{0}_U1.app.msgSQ = {2}
+**H_{0}_U1.app.msg2msgGap = {3}ns
+**H_{0}_U1.app.msgLength = {4}B
+##-------------------------------------
+"""
+class App:
+    def __init__(self, id, sq, gap, length):
+        self.id = id
+        self.sq = sq
+        self.gap = gap
+        self.length = length
+    def __str__(self):
+        return "id: {}, sq: {}, gap: {}, length: {}".format(self.id, self.sq, self.gap, self.length)
+
+gaps = [1, 10, 100, 1000]
+lengths = [512, 512*8, 512*32, 512*128, 512*512, 512*2048]
+
+
+apps = []
+id = 1
+sq = 1
+
+for gap in gaps:
+    for length in lengths:
+        apps.append(App(id, sq, gap, length))
+        id += 1
+
+for app in apps:
+    disable = 1
+    if str(app.id) == active_app or active_app == "-1":
+        disable = 0
+    print(template.format(app.id, disable, app.sq, app.gap, app.length))
+
---
+++ sim/examples/L1_S24/profile.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+for app in `seq 2 24`; do
+    ./app_gen.py $app > L1_S24.apps.ini        
+    for i in `seq 1 10`; do
+        bw=$(python -c "print($i * 0.1)")
+        sed "s/available_bw = 1.0/available_bw = ${bw}/g" L1_S24.ned_template > L1_S24.ned
+        echo "Started: " $app " with bw: " $i"0%" 
+        $d/src/ib_flit_sim_dbg -f L1_S24.ini -u Cmdenv > results/"${app}"_$i
+        echo "Finished: " $app " with bw: " $i"0%" 
+    done
+done
